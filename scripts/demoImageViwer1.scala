83ba37f0511863e947a5771a200957bc10d4765a
CaioRS <caiorss.rodrigues@gmail.com>
2017-06-29 15:18:43 -0300

    Add new example.

---
+++ scripts/demoImageViwer1.scala
@@ -0,0 +1,7 @@
+import jswing.Dialog.FileChooser
+
+val chooser = new FileChooser(title="Select an image").withImageFilter()
+val viwer = new jswing.guis.PictureFrame(visible = true)
+viwer.onClick{
+  chooser.run() foreach (img => viwer.setImageFromFile(img, 300))
+}
