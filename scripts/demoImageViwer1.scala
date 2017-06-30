a4284e4320afe44060d530a80e570b14dc988cef
CaioRS <caiorss.rodrigues@gmail.com>
2017-06-30 13:13:30 -0300

    Update demonstration file demoImageViwer1.scala.

--- scripts/demoImageViwer1.scala
+++ scripts/demoImageViwer1.scala
@@ -1,7 +1,10 @@
 import jswing.Dialog.FileChooser
 
 val chooser = new FileChooser(title="Select an image").withImageFilter()
-val viwer = new jswing.guis.PictureFrame(visible = true)
+val viwer = new jswing.guis.PictureFrame(
+  visible     = true
+ ,exitOnClose = true   
+)
 viwer.onClick{
   chooser.run() foreach (img => viwer.setImageFromFile(img, 300))
 }
