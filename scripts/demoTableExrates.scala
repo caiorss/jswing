da42ef8052bbf822789708903f13b41f2b9e9e0e
CaioRS <caiorss.rodrigues@gmail.com>
2017-07-06 08:37:15 -0300

    Update script demoTableExrates.scala.

--- scripts/demoTableExrates.scala
+++ scripts/demoTableExrates.scala
@@ -54,16 +54,35 @@ frame.setResizable(false)
 frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE)
 frame.setVisible(true)
 
+
+def runEvery(period: Int)(action: => Unit) = {
+  val listener = new java.awt.event.ActionListener(){
+    def actionPerformed(ev: java.awt.event.ActionEvent) = action
+  }
+  val timer = new javax.swing.Timer(period, listener)
+  timer.start()
+  timer
+}
+
 def updateRates() = { 
   val rates = Future { getExchangeRates() }
   statusLabel.setText("Update exchange rates ...")
+
   rates onSuccess { case r =>
+    model.clear()
     model.addItems(r)
     statusLabel.setText("Last update " + new java.util.Date())
     println("Exchange rates updated")
+  }
+
+  rates onFailure { case ex =>
+    println("Error: Couldn't fetch exchange rates")
+    println("Error: " + ex)
+    jswing.Dialog.showError("Error report.", "Error: Couldn't fetch exchange rates\nError: " + ex)
   }
 }
 
 Event.onButtonClick(buttonUpdate){ updateRates() }
 
-updateRates()
+/// Update exchange rate every 10 seconds 
+// runEvery(10000){ updateRates() }
