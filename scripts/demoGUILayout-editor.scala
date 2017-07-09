808e4db5fe07b9415c3062f5160ca351db9c68d3
CaioRS <caiorss.rodrigues@gmail.com>
2017-07-08 23:33:48 -0300

    Add new demo scripts/demoGUILayout-editor.scala.

---
+++ scripts/demoGUILayout-editor.scala
@@ -0,0 +1,19 @@
+import jswing.layout.Builder
+import jswing.Event
+
+val gui = Builder.makeFromFile("scripts/gui-borderlayout1.xml")
+val btnOpen = Builder.getJButton(gui, "btnOpen")
+val btnClear = Builder.getJButton(gui, "btnClear")
+val tarea = Builder.getJTextArea(gui, "tarea")
+
+val fchooser = new jswing.Dialog.FileChooser().withHome()
+
+def readFile(file: String) =
+  scala.io.Source.fromFile(file).mkString
+
+Event.onButtonClick(btnClear){ tarea.setText("") }
+
+Event.onButtonClick(btnOpen){
+
+  fchooser.run() foreach { file => tarea.setText(readFile(file))}
+}
