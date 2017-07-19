33e7d5d083e32e0d1678ee3de3c88f6de3f6e453
CaioRS <caiorss.rodrigues@gmail.com>
2017-07-19 06:50:44 -0300

    Add demo demoBorderPanel.scala.

---
+++ demo/demoBorderPanel.scala
@@ -0,0 +1,54 @@
+import jswing.{Event, Dialog}
+import jswing.widgets.{Frame, BorderPanel}
+import javax.swing._
+
+val fdialog = new jswing.Dialog.FileChooser().withHome()
+
+val panel = new BorderPanel()
+
+val topPanel    = new JPanel()
+val openButton  = new JButton("Open")
+val clearButton = new JButton("Clear")
+val exitButton  = new JButton("Exit")
+
+
+topPanel.add(openButton)
+topPanel.add(clearButton)
+topPanel.add(exitButton)
+
+panel.addTop(topPanel)
+
+val tarea = new JTextArea()
+panel.addCenterScrollPane(tarea)
+
+val statusBar = new JLabel("Status bar")
+panel.addBottom(statusBar)
+
+val frame = new Frame(
+  title       = "BorderLayout test",
+  size        = (500, 400),
+  // resizable   = false,
+  pane        = panel,
+  visible     = true,
+  exitOnClose = true
+)
+
+def readFile(file: String) =
+  scala.io.Source.fromFile(file).mkString
+
+
+Event.onButtonClick(openButton){
+  val file = fdialog.run()
+  file map readFile foreach tarea.setText
+  file foreach {f => statusBar.setText("Editing file: " + f)}
+}
+
+Event.onButtonClick(clearButton){
+  tarea.setText("")
+  statusBar.setText("Editing file:")
+}
+
+Event.onButtonClick(exitButton){
+  System.exit(0)
+}
+
