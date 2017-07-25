fd9d7df1bf82840f1ede50ead00b24e750a81294
CaioRS <caiorss.rodrigues@gmail.com>
2017-07-25 16:12:46 -0300

    Add class ListBox's documentation and example.

--- src/jswing.widgets.scala
+++ src/jswing.widgets.scala
@@ -188,6 +188,64 @@ class ComboBoxValue[A] extends javax.swing.JComboBox[ComboItem[A]] {
 }
 
 
+/** 
+    JList extension that allows label and value items. This class also provides 
+    type safe and null-safe methods to get selected label and selected items.
+
+    Example: 
+
+    {{{
+    import jswing.widgets.{BorderPanel, ListBox}
+    import jswing.{Event, Dialog}
+    import javax.swing._
+
+    val dirch = new Dialog.DirChooser().withHome()
+
+    val frame = new JFrame("Listbox test")
+    frame.setSize(500, 400)
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+
+    val buttonOpen = new JButton("Open")
+    val buttonClear = new JButton("Clear")
+
+    val display = new JLabel("")
+    val lbox = new ListBox[String]()
+
+    val panel = new BorderPanel()
+    panel.addTopItems(buttonOpen, buttonClear)
+    panel.addCenterScrollPane(lbox)
+    panel.addBottomItems(display)
+
+    def showFiles(path: String){
+      lbox.clear()
+      display.setText("")
+      jswing.JUtils.invokeLater{
+        val files = new java.io.File(path).listFiles()
+        files foreach {f => lbox.addItem(f.getName(), f.getAbsolutePath())}
+      }
+    }
+
+    Event.onButtonClick(buttonOpen){
+      dirch.run() foreach showFiles
+    }
+
+    Event.onButtonClick(buttonClear){
+      lbox.clear()
+      display.setText("")
+    }
+
+    lbox.onSelect{
+      println("Selected label = " + lbox.getSelectedItemLabel())
+      println("Selected value = " + lbox.getSelectedItemValue())
+      lbox.getSelectedItemValue foreach display.setText
+    }
+
+
+    frame.setContentPane(panel)
+    frame.setVisible(true)
+    }}}
+
+  */
 class ListBox[A] extends javax.swing.JList[ComboItem[A]] {
   private val model  = new javax.swing.DefaultListModel[ComboItem[A]]()
 
